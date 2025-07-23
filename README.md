5fdbe79adb1f3e1c72cd02d982778361925784fa
Vijay Ganesh <Vijay.Ganesh0811@gmail.com>
2025-07-23 10:36:19 +0530

    Consuming JSON Object.

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Contains Basic Java SpringBoot Application as Kafka Consumer
 
 - @KafkaListener is used to listen to specific topic
 - Group-Id is mandatory to consume from a Topic
+
+## Consuming JSON Object
+- Below properties to be added in the application.properties file in-order to read the JSON Object from Kafka Topic
+- spring.kafka.consumer.key-deserializer=org.apache.kafka.common.serialization.StringDeserializer
+- spring.kafka.consumer.value-deserializer=org.springframework.kafka.support.serializer.JsonDeserializer
+- spring.kafka.consumer.properties.spring.json.trusted.packages=com.training.model
+- The configuration can also be done via Java Class files. its given in config\EventConsumerConfig class file.
--- src/main/java/com/training/consumer/MessageListener.java
+++ src/main/java/com/training/consumer/MessageListener.java
@@ -1,5 +1,6 @@
 package com.training.consumer;
 
+import com.training.model.Customer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,17 @@ import org.springframework.stereotype.Service;
 public class MessageListener {
 
 
-    @KafkaListener(topics = "kafkatraining-topic5", groupId = "ConsumerGroup1")
-    public void consumeMessage(String message) {
+//    @KafkaListener(topics = "kafkatraining-topic4", groupId = "ConsumerGroup1")
+//    public void consumeMessage(String message) {
+//
+//        log.info("Consumer consumed the Message : {}", message);
+//
+//    }
 
-        log.info("Consumer consumed the Message : {}", message);
+    @KafkaListener(topics = "kafkatraining-topic5", groupId = "ConsumerGroup1")
+    public void consumeEventMessage(Customer customer) {
+
+        log.info("Consumer consumed the Event Message : {}", customer.toString());
 
     }
 }
